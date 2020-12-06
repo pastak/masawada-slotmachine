a103090228e263cac16a16856b32621f160981f2
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-12-06 19:48:57 +0900

    eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
-function HomePage() {
-    return <div>Welcome to Next.js!</div>
+import React from "react";
+
+const HomePage: React.FC = () => {
+   return <div>Welcome to Next.js!</div>
 }
-  
+
 export default HomePage
