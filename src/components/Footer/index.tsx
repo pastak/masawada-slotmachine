dfe73cc71cffaf8b1ed4b9574b1935ec83fdf34e
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-12-07 01:04:14 +0900

    footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Footer: React.FC = () => (
+  <address>
+    masawada slot by <a href="https://github.com/astj">astj</a>. more details at{" "}
+    <a href="https://blog.astj.space/entry/2020/12/06/235824">
+      ma→sa→wa→da で masawada 完成 (masawada slotmachine 第1回) - 平常運転
+    </a>
+  </address>
+);
--- src/pages/slots/index.tsx
+++ src/pages/slots/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import { Slotmachine } from "../../components/Slotmachine";
+import { Footer } from "../../components/Footer";
 
 const SlotPage: React.FC = () => {
   const symbols = ["ma", "sa", "wa", "da"] as const;
@@ -15,18 +16,7 @@ const SlotPage: React.FC = () => {
       <hr />
       <Slotmachine symbols={symbols} />
       <hr />
-      <dl>
-        <dt>author</dt>
-        <dd>
-          <a href="https://github.com/astj">astj</a>
-        </dd>
-        <dt>what's this?</dt>
-        <dd>
-          <a href="https://blog.astj.space/entry/2020/12/06/235824">
-            masawada advent calender 2020 12/06
-          </a>
-        </dd>
-      </dl>
+      <Footer />
     </div>
   );
 };
