c419ff4f74ac34c5a9c38b24cad8a6285ed3988b
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-12-07 01:19:45 +0900

    eslint about react

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,10 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
 };
