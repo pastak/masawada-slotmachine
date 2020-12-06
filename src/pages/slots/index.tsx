bcafff48c798b07b961fc35ae73fc7b1c205a9bd
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-12-06 23:44:06 +0900

    it works!

--- src/pages/slots/index.tsx
+++ src/pages/slots/index.tsx
@@ -8,6 +8,8 @@ const SlotPage: React.FC = () => {
         <title>masawada slot!!!</title>
       </Head>
       <h1>masawada slot!!</h1>
+      <hr />
+      <Slotmachine />
     </div>
   );
 };
@@ -23,23 +25,37 @@ type SlotProps = Readonly<{
 }>;
 
 const Slotmachine: React.FC = () => {
+  const [machineState, setMachineState] = useState<"rolling" | "stopped">(
+    "stopped"
+  );
   const [stateMa, setStateMa] = useState<SlotProps["state"]>("stopped");
   const [stateSa, setStateSa] = useState<SlotProps["state"]>("stopped");
   const [stateWa, setStateWa] = useState<SlotProps["state"]>("stopped");
   const [stateDa, setStateDa] = useState<SlotProps["state"]>("stopped");
-  const results = useRef<number[]>([]);
+  const results = useRef<(number | undefined)[]>([]);
   const checkResult = () => {
     if (
-      results.current[0] == 0 &&
-      results.current[1] == 1 &&
-      results.current[2] == 2 &&
-      results.current[3] == 3
+      results.current[0] == undefined ||
+      results.current[1] == undefined ||
+      results.current[2] == undefined ||
+      results.current[3] == undefined
+    ) {
+      return;
+    }
+    if (
+      results.current[0] === 0 &&
+      results.current[1] === 1 &&
+      results.current[2] === 2 &&
+      results.current[3] === 3
     ) {
       // success!
       window.alert("success!");
-      // reset status
-      results.current = [];
+    } else {
+      window.alert("残念でした！");
     }
+    // reset status
+    results.current = [];
+    setMachineState("stopped");
   };
   const onUpdateMa = useCallback(
     (index: number) => {
@@ -49,26 +65,88 @@ const Slotmachine: React.FC = () => {
     },
     [checkResult]
   );
+  const onUpdateSa = useCallback(
+    (index: number) => {
+      setStateSa("stopped");
+      results.current[1] = index;
+      checkResult();
+    },
+    [checkResult]
+  );
+  const onUpdateWa = useCallback(
+    (index: number) => {
+      setStateWa("stopped");
+      results.current[2] = index;
+      checkResult();
+    },
+    [checkResult]
+  );
+  const onUpdateDa = useCallback(
+    (index: number) => {
+      setStateDa("stopped");
+      results.current[3] = index;
+      checkResult();
+    },
+    [checkResult]
+  );
 
   const start = () => {
+    setMachineState("rolling");
     setStateMa("rolling");
     setStateSa("rolling");
     setStateWa("rolling");
     setStateDa("rolling");
   };
-  return <div></div>;
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <Slot initialIndex={0} state={stateMa} onStop={onUpdateMa} />
+            </td>
+            <td>
+              <Slot initialIndex={1} state={stateSa} onStop={onUpdateSa} />
+            </td>
+            <td>
+              <Slot initialIndex={2} state={stateWa} onStop={onUpdateWa} />
+            </td>
+            <td>
+              <Slot initialIndex={3} state={stateDa} onStop={onUpdateDa} />
+            </td>
+          </tr>
+          <tr>
+            <td rowSpan={4}>
+              <button
+                onClick={() => start()}
+                disabled={machineState === "rolling"}
+              >
+                start!!
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
+
 const Slot: React.FC<SlotProps> = ({ initialIndex, state, onStop }) => {
   const interval = useRef<ReturnType<Window["setInterval"]> | undefined>(
     undefined
   );
   const [index, setIndex] = useState<number>(initialIndex);
+  const indexRef = useRef<number>(initialIndex);
+  useEffect(() => {
+    indexRef.current = index;
+  }, [index]);
   const setNextIndex = useCallback(() => {
-    if (index > slotCandidates.length) {
-      setIndex(0);
-    } else {
-      setIndex(index + 1);
+    let nextIndex = indexRef.current + 1;
+    if (nextIndex >= slotCandidates.length) {
+      nextIndex = 0;
     }
+    setIndex(nextIndex);
+    //    indexRef.current = nextIndex;
   }, []);
 
   useEffect(() => {
@@ -90,8 +168,11 @@ const Slot: React.FC<SlotProps> = ({ initialIndex, state, onStop }) => {
 
   return (
     <div>
-      <input readOnly value={slotCandidates[index]} />
-      <input type="button" onClick={() => stop()} />
+      <span>{slotCandidates[index]}</span>
+      <br />
+      <button onClick={() => stop()} disabled={state === "stopped"}>
+        stop
+      </button>
     </div>
   );
 };
